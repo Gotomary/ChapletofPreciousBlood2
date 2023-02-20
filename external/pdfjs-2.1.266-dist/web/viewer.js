68f7b4aad5ff3f71573029bd011d07249bd288f5
Gotomary <26889132+Gotomary@users.noreply.github.com>
2023-02-20 13:39:05 +1100

    Update viewer.js

--- external/pdfjs-2.1.266-dist/web/viewer.js
+++ external/pdfjs-2.1.266-dist/web/viewer.js
@@ -5132,7 +5132,7 @@ var defaultOptions = {
     kind: OptionKind.VIEWER
   },
   defaultUrl: {
-    value: 'How to pray the rosary.pdf',
+    value: 'A short guide on how to pray the Chaplet and Litany of the Most Precious Blood of Jesus using Rosary beads.pdf',
     kind: OptionKind.VIEWER
   },
   defaultZoomValue: {
