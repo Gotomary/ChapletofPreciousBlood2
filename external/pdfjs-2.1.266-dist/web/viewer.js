616f394f2d383f92a9726f9f6b241af16b2de983
Gotomary <26889132+Gotomary@users.noreply.github.com>
2023-02-27 18:50:31 +1100

    Update viewer.js

--- external/pdfjs-2.1.266-dist/web/viewer.js
+++ external/pdfjs-2.1.266-dist/web/viewer.js
@@ -5132,7 +5132,7 @@ var defaultOptions = {
     kind: OptionKind.VIEWER
   },
   defaultUrl: {
-    value: 'Precious blood of Christ.PDF',
+    value: 'Precious blood of Christ.pdf',
     kind: OptionKind.VIEWER
   },
   defaultZoomValue: {
